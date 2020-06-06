00c47e77df1009afadb1e2926ed8d310c11f5829
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-07 01:59:45 +0300

    Created TypeScript Node Express

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Commands
+To Start
+`npm run start`
+
+To Start Development Nodemon
+`npm run dev`
+
+To Build from Typescript 
+`npm run buil`
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-const greeting: string = 'Hello there';
+import express, {Application, Request, Response} from 'express';
 
-const numbers: number[] = [1,2,3,4]
+const app: Application = express();
+const port: number = 3000;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Server is Working');
+});
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+})
